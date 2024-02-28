5dc4ed7a58e94de28e183fccf3b0e59abea1001f
EKL-NCI <114241384+EKL-NCI@users.noreply.github.com>
2024-02-28 17:56:10 +0000

    edited .py file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, rendertemplate, urlfor
-app = Flask(name)
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return rendertemplate('index.html')  # This will render the HTML file with the PubNub subscription.
+    return render_template('index.html')  # This will render the HTML file with the PubNub subscription.
 
-if _name == '__main':
+if __name__ == '__main__':
     app.run(debug=True)
