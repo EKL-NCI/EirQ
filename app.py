03e4127705ab18016ee872b0f4076450bff61c9b
EKL-NCI <114241384+EKL-NCI@users.noreply.github.com>
2024-03-11 09:46:25 +0000

    - Morris DB Changes - ✨ added credentials.json ✨ Database Intro to backend

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template, url_for, request, redirect
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
+import firebaseadmin
+from firebaseadmin import credentials, db
 
 # Firebase info will go here !
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://eirq-solutions-default-rtdb.europe-west1.firebasedatabase.app/'%7D)
 
 app = Flask(__name__)
 
@@ -16,7 +20,11 @@ messages = []
 
 class MySubscribeCallback(SubscribeCallback):
     def message(self, pubnub, message):
+        # Append received message to the messages list
         messages.append(message.message)
+        # Stores the received message in Firebase Realtime Database
+        ref = db.reference('air_quality')
+        ref.push(message.message)
 
 # Adding the listener and subscribing to the channel
 pubnub.add_listener(MySubscribeCallback())
