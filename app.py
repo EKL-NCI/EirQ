70fbf097fc33fb0dec2ae283ac3c57a0a1196b56
EKL-NCI <114241384+EKL-NCI@users.noreply.github.com>
2024-03-18 17:16:01 +0000

    🔥made database.db file - for users (not working as of now)  ✨ added some code for users (not fully set up or working as of now)  🗃️ fixed some firebase import syntax

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
-import firebaseadmin
-from firebaseadmin import credentials, db
+import firebase_admin
+from firebase_admin import credentials, db
 
 # Firebase info will go here !
 cred = credentials.Certificate("credentials.json")
-firebase_admin.initialize_app(cred,'databaseURL': 'https://eirq-solutions-default-rtdb.europe-west1.firebasedatabase.app/'%7D)
+try:
+    firebase_admin.initialize_app(cred, name="sensors", options={'databaseURL': 'https://eirq-solutions-default-rtdb.europe-west1.firebasedatabase.app/'})
+except ValueError as e:
+    print("Error initializing Firebase:", e)
 
+# App instance
 app = Flask(__name__)
+
+
+# Connects app file to DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# Secret key for the app
+app.config['SECRET_KEY'] = 'admin'
+# User Database Innstance
+db_users = SQLAlchemy(app)
+
+class User(db_users.Model, UserMixin):
+    id = db_users.Column(db_users.Integer, primary_key=True)
+    username = db_users.Column(db_users.String(20), unique=True, nullable=False)
+    password = db_users.Column(db_users.String(80), nullable=False)
+
 
 # PubNub configuration
 pnconfig = PNConfiguration()
@@ -23,14 +43,15 @@ class MySubscribeCallback(SubscribeCallback):
         # Append received message to the messages list
         messages.append(message.message)
         # Stores the received message in Firebase Realtime Database
-        ref = db.reference('air_quality')
+        ref = db.reference('/air_quality')
         ref.push(message.message)
 
 # Adding the listener and subscribing to the channel
-pubnub.add_listener(MySubscribeCallback())
-pubnub.subscribe().channels('aq_channel').execute()
+def subscribe_to_channel():
+    pubnub.add_listener(MySubscribeCallback())
+    pubnub.subscribe().channels('aq_channel').execute()
 
-print("Messages: ", messages)
+# Routing to pages
 
 @app.route('/')
 def index():
@@ -48,5 +69,6 @@ def sensors():
 def sign_up():
     return render_template('Signup.html')
 
+# Will catch any 404 error
 if __name__ == '__main__':
     app.run(debug=True)
